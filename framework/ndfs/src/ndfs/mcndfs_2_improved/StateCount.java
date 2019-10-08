a355e662cd85d8ed244c6d71bbb2540be6b8820f
Soufiane Jounaid <soufianejounaid@gmail.com>
2019-10-08 18:36:51 +0200

    Fixed Deadlock issue
    added timeouts to .wait() and cleaned up, performed first round of testing with this.

--- framework/ndfs/src/ndfs/mcndfs_2_improved/StateCount.java
+++ framework/ndfs/src/ndfs/mcndfs_2_improved/StateCount.java
@@ -2,12 +2,14 @@ package ndfs.mcndfs_2_improved;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+
 import graph.State;
 
 public class StateCount{ // Singleton class, to be used as a shared object
 
     private static StateCount singleInstance = new StateCount();
     private static final ConcurrentHashMap<State, Counter> map = new ConcurrentHashMap<State, Counter>();
+
 
     public static StateCount getInstance(){
         return singleInstance;
